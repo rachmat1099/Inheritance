878738d37577118010539473b30ce47d879709b9
Rahmat <61777573+rachmat1099@users.noreply.github.com>
2020-04-10 11:42:59 +0800

    Inheritance

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Rachmat", 20);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Kamarudin", 35, "190302", "Pemrograman");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Rachmat", 20, "19.11.2638", "rachmat.rachmat@students.@amikom.ac.id");
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+
+        }
+    }
+}
